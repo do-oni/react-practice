28b4af07069cc49a59b82d4e36a98f4f8067f041
do-oni <dooniseo@gmail.com>
2022-03-05 15:52:51 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
+
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  const onShow = () => setShowing((prev) => !prev);
+
+  function Hello() {
+    useEffect(() => {
+      console.log("hi :)");
+      return () => console.log("bye :(");
+    }, []);
+    return <h1>Hello</h1>;
+  }
+
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+
+  useEffect(() => {
+    if (counter !== "" && keyword.length > 0) {
+      console.log("I run when 'counter' changes");
+    }
+  }, [counter]);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 0) {
+      console.log("I run when 'keyword' changes");
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes");
+  }, [keyword, counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <hr />
+      <button onClick={onShow}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
